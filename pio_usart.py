4c0a51367549bd9f5bb400107a83b0ef95a875af
koda1026 <koda1026@h-ka.de>
2023-05-19 01:24:29 +0200

    Untested version of rudementary sender and reciever.

--- pio_usart.py
+++ pio_usart.py
@@ -2,14 +2,13 @@ from machine import Pin
 from time import sleep
 import rp2
 
-@rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_init=rp2.PIO.OUT_LOW)
+@rp2.asm_pio(out_init=rp2.PIO.OUT_LOW)
 def tx():
     
     pull()
 
-    wrap_target()   #start
-    
-    wait(1, pins, 0)
+    wrap_target()   # start
+    wait(1, pins, 25)
     out(pins, 1)
     nop()           [7]
     nop()           [7]
@@ -20,6 +19,22 @@ def tx():
     nop()           [7]
     nop()           [7]
     
+    wrap()
+    
+@rp2.asm_pio()
+def rx():
+    wrap_target()   # start
+    
+    
+    set(x, 8)
+    label('single_frame')
+    
+    wait(1, pins, 25)
+    in_(pins, 1)
+    
+    jmp(x_dec, 'single_frame')
+    
+    push
     wrap()
     
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
@@ -37,13 +52,20 @@ def cl():
     wrap()
    
 
-sm_tx = rp2.StateMachine(0, tx, freq=2000, in_base=Pin(0), out_base=Pin('LED'), out_shiftdir=rp2.PIO.SHIFT_RIGHT)
-sm_cl = rp2.StateMachine(1, cl, freq=2000, set_base=Pin(0))
+sm_tx = rp2.StateMachine(0, tx, freq=2000, out_base=Pin(0), out_shiftdir=rp2.PIO.SHIFT_RIGHT)
+sm_rx = rp2.StateMachine(1, tx, freq=2000, in_base=Pin(1), in_shiftdir=rp2.PIO.SHIFT_RIGHT)
+sm_cl = rp2.StateMachine(2, cl, freq=2000, set_base=Pin('LED'))
 
 sm_tx.active(1)
+sm_rx.active(1)
 sm_cl.active(1)
+
 sm_tx.put(5461) #0b1010101010101
 sleep(3.0)
-sm_tx.active(0)
+z = sm_rx.get()
+
 sm_cl.active(0)
-print('Bye')
+sm_rx.active(0)
+sm_tx.active(0)
+
+print(z)
